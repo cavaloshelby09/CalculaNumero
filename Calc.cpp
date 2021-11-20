67cd20a98e0cab026fa410fc0391bee63616f093
cavaloshelby09 <87613130+cavaloshelby09@users.noreply.github.com>
2021-11-20 10:12:26 -0300

    Add files via upload

---
+++ Calc.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string UserInput;
+double Resultado;
+double UserNumberInput;
+double UserNumberInput2;
+string HoldExit;
+
+
+
+    cout << "Bem vindo para a calculadora 100% profissional!" << endl;
+    cout << "Selecione o calculo que voce quer fazer: Adicao[1], Subtracao[2], Vezes[3], Divisao[4]" << endl;
+    cin >> UserInput;
+
+    if (UserInput == "1")
+    {
+        cout << "Coloque os numeros(Exemplo:10.0 e 20.0)" << endl;
+        cin >> UserNumberInput;
+        cout << "Proximo numero" << endl;
+        cin >> UserNumberInput2;
+        Resultado = UserNumberInput + UserNumberInput2;
+        cout << "Resultado:" << Resultado << endl;
+        cin >> HoldExit;
+    }
+
+
+    else if (UserInput == "2")
+    {
+        cout << "Coloque os numeros(Exemplo:10.0 e 20.0)" << endl;
+        cin >> UserNumberInput;
+        cout << "Proximo numero" << endl;
+        cin >> UserNumberInput2;
+        Resultado = UserNumberInput - UserNumberInput2;
+        cout << "Resultado:" << Resultado << endl;
+        cin >> HoldExit;
+    }
+
+
+    else if (UserInput == "3")
+    {
+        cout << "Coloque os numeros(Exemplo:10.0 e 20.0)" << endl;
+        cin >> UserNumberInput;
+        cout << "Proximo numero" << endl;
+        cin >> UserNumberInput2;
+        Resultado = UserNumberInput * UserNumberInput2;
+        cout << "Resultado:" << Resultado << endl;
+        cin >> HoldExit;
+    }
+
+
+    else if (UserInput == "4")
+    {
+        cout << "Coloque os numeros(Exemplo:10.0 e 20.0)" << endl;
+        cin >> UserNumberInput;
+        cout << "Proximo numero" << endl;
+        cin >> UserNumberInput2;
+        Resultado = UserNumberInput / UserNumberInput2;
+        cout << "Resultado:" << Resultado << endl;
+        cin >> HoldExit;
+    }
+
+
+    else if (UserInput == "5")
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            cout << "SUS SUS SUS AMOGUS AMOGUS AMOGUS EASTER EGG EASTER EGG EASTER EASTER EASTER EASTER EASTER EASTER";
+        }
+        cin >> HoldExit;
+    }
+    return 0;
+}
